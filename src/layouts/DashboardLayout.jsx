6ef14743f3a62fe421eb9ef432285aa1ca8c9750
Miguel Portillo <mipor.278@gmail.com>
2022-11-21 12:00:01 -0500

    Un pequeño cambio

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -11,12 +11,12 @@ const DashboardLayout = ({ children }) => {
         {/* Sidebar */}
         <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
           {/* Sidebar - Brand */}
-          <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+          <Link className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
             <div className="sidebar-brand-icon rotate-n-15">
             <i className="fas fa-wave-square"></i>
             </div>
             <div className="sidebar-brand-text mx-3">Dashboard IoT</div>
-          </a>
+          </Link>
           {/* Divider */}
           <hr className="sidebar-divider my-0" />
           {/* Nav Item - Dashboard */}
